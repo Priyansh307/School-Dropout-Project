80fbe25224f78d12a69575e0c3fd86041bff315c
CodeMonk64 <118073376+CodeMonk64@users.noreply.github.com>
2024-03-05 00:15:36 +0530

    Update Student_full_details.jsx

--- Project/src/Components/School/Student_full_details.jsx
+++ Project/src/Components/School/Student_full_details.jsx
@@ -1,28 +1,25 @@
 import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import './Std_full_details.css'
-
+import './Std_full_details.css';
+import Navbarschmo from './components2/Navbarschmo';
+import { Route, Routes } from 'react-router-dom'; 
+import Home from './Pages2/Home';
+import Product from './Pages2/Product';
+import Report from './Pages2/Report';
 
 const Student_full_details = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const { rollno, name, address, standard, income, district } = location.state;
-
-  const goBack = () => {
-    navigate("/loginschhome");
-  };
-
   return (
-    <div className='std_full_detail_card'>
-      <p>RollNo :- {rollno}</p>
-      <p>Name :- {name}</p>
-      <p>Address:- {address}</p>
-      <p>Standard:- {standard}</p>
-      <p>Income:- {income}</p>
-      <p>District:- {district}</p>
-      <button className='full_det_btn' onClick={goBack}>Go Back</button>
-    </div>
+    <>
+      <Navbarschmo />
+      <Routes> 
+      
+          <Route path='/student-details/home' element={<Home />} /> 
+          <Route path='/student-details/products' element={<Product />} />
+          <Route path='/student-details/report' element={<Report />} />
+      </Routes>
+    </>
   );
 }
 
 export default Student_full_details;
+
+
