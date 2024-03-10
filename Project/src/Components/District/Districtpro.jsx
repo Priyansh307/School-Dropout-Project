f1b43d7c9b9819efc925a5d2a09a91e5b2a4e91d
Dhyanam Pathak <dhyanam464@gmail.com>
2024-03-10 11:31:14 +0530

    updated profiles

--- Project/src/Components/District/Districtpro.jsx
+++ Project/src/Components/District/Districtpro.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import '../District/Districtpro.css'
 
 const Districtpro = () => {
+    const [school, setSchool] = useState(null);
+
+    useEffect(() => {
+      // Fetch school data from the server
+      const fetchSchool = async () => {
+        try {
+          const token = localStorage.getItem("token");
+          const response = await axios.get("http://localhost:3000/auth/school", {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          setSchool(response.data.schools[0]); // Assuming there's only one school per user
+        } catch (error) {
+          console.error(error);
+        }
+      };
+  
+      fetchSchool();
+    }, []);
+
   return (
     <div className="container_district">
     <div className="sub-container_district">
@@ -9,15 +31,15 @@ const Districtpro = () => {
         <table className='table_goverment'>
             <tr>
                 <td className="t_title_distict"> District Name </td>
-                <td>Rajkot</td>
+                <td>{school.username}</td>
             </tr>
             <tr>
                 <td className="t_title_district"> District email </td>
-                <td>rajkot@gmail.com</td>
+                <td>{school.email}</td>
             </tr>
             <tr>
                 <td className="t_title_district"> District State </td>
-                <td>Gujarat</td>
+                <td>{school.state}</td>
             </tr>
             
             
